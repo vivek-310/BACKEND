427061dcc98fced2d55edf01322ee07771362df9
vivek chinni <vivekvardhan0310@gmail.com>
2024-05-10 10:20:44 +0530

    introduction to npm

---
+++ 2.npm/index.js
@@ -0,0 +1,13 @@
+// var generateName = require('sillyname');
+// var x = generateName();
+
+// console.log(`my name is ${x}.`);
+
+var superHero=require('superheroes');
+//import superHero from 'superheroes';
+var y=superHero.random();
+console.log(`iam ${y}`);
+
+
+
+
